aa2d3d08309b22c04a269fc6e0642052a7622094
Jacob Wegner <jwegner@eldarion.com>
2019-11-13 16:10:06 -0600

    backport reference filter from feature/translation-alignments

--- readhomer_atlas/library/schema.py
+++ readhomer_atlas/library/schema.py
@@ -1,3 +1,6 @@
+from django.db.models import Q, Min, Max
+
+import django_filters
 from graphene import ObjectType, String, relay
 from graphene.types import generic
 from graphene_django import DjangoObjectType
@@ -66,13 +69,46 @@ class BookNode(DjangoObjectType):
         filter_fields = ["position", "version__urn"]
 
 
+class LineFilterSet(django_filters.FilterSet):
+    reference = django_filters.CharFilter(method="reference_filter")
+
+    class Meta:
+        model = Line
+        fields = ["position", "book__position", "version__urn"]
+
+    def reference_filter(self, queryset, name, value):
+        try:
+            start, end = value.split("-")
+        except ValueError:
+            start = end = value
+        # @@@ further validation required
+        try:
+            start_book, start_line = start.split(".")
+        except ValueError:
+            # @@@ handle "1"
+            return queryset.none()
+        try:
+            end_book, end_line = end.split(".")
+        except ValueError:
+            # @@@ handle "1.1-2"
+            return queryset.none()
+
+        subquery = queryset.filter(
+            Q(book__position=start_book, position=start_line)
+            | Q(book__position=end_book, position=end_line)
+        ).aggregate(min=Min("idx"), max=Max("idx"))
+        queryset = queryset.filter(idx__gte=subquery["min"], idx__lte=subquery["max"])
+        # @@@ select related required for performant `label`
+        return queryset.select_related("book")
+
+
 class LineNode(DjangoObjectType):
     label = String()
 
     class Meta:
         model = Line
         interfaces = (relay.Node,)
-        filter_fields = ["book__position", "version__urn"]
+        filterset_class = LineFilterSet
 
 
 class Query(ObjectType):
