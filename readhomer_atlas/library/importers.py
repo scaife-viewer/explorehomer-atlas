656fa2c587e341f73ae74c4d91c97a05df12924c
Jacob Wegner <jwegner@eldarion.com>
2020-01-15 17:26:04 -0600

    factor out LibraryDataResolver

--- readhomer_atlas/library/importers.py
+++ readhomer_atlas/library/importers.py
@@ -13,6 +13,41 @@ from .urn import URN
 LIBRARY_DATA_PATH = os.path.join(settings.PROJECT_ROOT, "data", "library")
 
 
+class LibraryDataResolver:
+    def __init__(self, data_dir_path):
+        self.text_groups = {}
+        self.works = {}
+        self.versions = {}
+
+        self.resolved = self.resolve_data_dir_path(data_dir_path)
+
+    def populate_versions(self, dirpath, data):
+        for version in data:
+            version_part = version["urn"].rsplit(":", maxsplit=2)[1]
+            version_path = os.path.join(dirpath, f"{version_part}.txt")
+
+            if not os.path.exists(version_path):
+                raise FileNotFoundError(version_path)
+
+            self.versions[version["urn"]] = {"path": version_path, **version}
+
+    def resolve_data_dir_path(self, data_dir_path):
+        for dirpath, dirnames, filenames in os.walk(data_dir_path):
+            if "metadata.json" not in filenames:
+                continue
+
+            metadata = json.load(open(os.path.join(dirpath, "metadata.json")))
+            assert metadata["node_kind"] in ["textgroup", "work"]
+
+            if metadata["node_kind"] == "textgroup":
+                self.text_groups[metadata["urn"]] = metadata
+            elif metadata["node_kind"] == "work":
+                self.works[metadata["urn"]] = metadata
+                self.populate_versions(dirpath, metadata["versions"])
+
+        return self.text_groups, self.works, self.versions
+
+
 class Library:
     def __init__(self, text_groups, works, versions):
         self.text_groups = text_groups
@@ -124,43 +159,7 @@ class CTSImporter:
 
 
 def resolve_library():
-    metadata_file_path = None
-
-    directories = []
-    for fname in os.listdir(LIBRARY_DATA_PATH):
-        full_path = os.path.join(LIBRARY_DATA_PATH, fname)
-        if fname == "metadata.json":
-            metadata_file_path = full_path
-        elif os.path.isdir(full_path):
-            directories.append(full_path)
-
-    text_groups = {}
-    subdirectories = []
-    for directory_path in directories:
-        for fname in os.listdir(directory_path):
-            full_path = os.path.join(directory_path, fname)
-            if fname == "metadata.json":
-                metadata_file_path = full_path
-                metadata = json.load(open(metadata_file_path))
-                text_groups[metadata["urn"]] = metadata
-            elif os.path.isdir(full_path):
-                subdirectories.append(full_path)
-    works = {}
-    versions = {}
-    for directory_path in subdirectories:
-        for fname in os.listdir(directory_path):
-            full_path = os.path.join(directory_path, fname)
-            if fname == "metadata.json":
-                metadata_file_path = full_path
-                metadata = json.load(open(metadata_file_path))
-                works[metadata["urn"]] = metadata
-                for version in metadata["versions"]:
-                    version_part = version["urn"].rsplit(":", maxsplit=2)[1]
-                    version_path = os.path.join(directory_path, f"{version_part}.txt")
-                    if not os.path.exists(version_path):
-                        raise FileNotFoundError(version_path)
-                    else:
-                        versions[version["urn"]] = {"path": version_path, **version}
+    text_groups, works, versions = LibraryDataResolver(LIBRARY_DATA_PATH).resolved
     return Library(text_groups, works, versions)
 
 
