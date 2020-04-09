bba4ef8687839fab692ca285b4d29b2929fa204d
Jacob Wegner <jwegner@eldarion.com>
2020-04-09 10:49:09 -0500

    ingest image annotations

--- readhomer_atlas/library/importers/__init__.py
+++ readhomer_atlas/library/importers/__init__.py
@@ -1,4 +1,10 @@
-from . import alignments, named_entities, text_annotations, versions
+from . import alignments, image_annotations, named_entities, text_annotations, versions
 
 
-__all__ = ["alignments", "named_entities", "text_annotations", "versions"]
+__all__ = [
+    "alignments",
+    "image_annotations",
+    "named_entities",
+    "text_annotations",
+    "versions",
+]
