3e7eafd597e5edaedf553a8ef198495b781fb528
Jacob Wegner <jwegner@eldarion.com>
2020-04-09 11:58:09 -0500

    fix current site issue

--- readhomer_atlas/web_annotation/shortcuts.py
+++ readhomer_atlas/web_annotation/shortcuts.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 
-CURRENT_SITE = Site.objects.get_current()
-
-
 def build_absolute_url(url):
+    # get_current should cache:
+    # https://docs.djangoproject.com/en/2.2/ref/contrib/sites/#caching-the-current-site-object
+    current_site = Site.objects.get_current()
     return "{scheme}://{host}{url}".format(
-        scheme=settings.DEFAULT_HTTP_PROTOCOL, host=CURRENT_SITE.domain, url=url
+        scheme=settings.DEFAULT_HTTP_PROTOCOL, host=current_site.domain, url=url
     )
